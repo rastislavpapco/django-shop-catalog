c222a6823e5bc717770a9c7c264efa2e562cadb7
Rastislav Papčo <rasto.pap@gmail.com>
2024-02-23 14:36:48 +0100

    Added BaseModel to models.

--- catalog/models.py
+++ catalog/models.py
@@ -1,26 +1,29 @@
 from django.db import models
 
 
-class AttributeName(models.Model):
+class BaseModel(models.Model):
     id = models.IntegerField(primary_key=True)
+
+    class Meta:
+        abstract = True
+
+
+class AttributeName(BaseModel):
     nazev = models.CharField(max_length=128, default="")
     kod = models.CharField(max_length=128, default="")
     zobrazit = models.BooleanField(default=True)
 
 
-class AttributeValue(models.Model):
-    id = models.IntegerField(primary_key=True)
+class AttributeValue(BaseModel):
     hodnota = models.CharField(max_length=128, default="")
 
 
-class Attribute(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Attribute(BaseModel):
     nazev_atributu_id = models.ForeignKey(AttributeName, on_delete=models.CASCADE)
     hodnota_atributu_id = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
 
 
-class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Product(BaseModel):
     nazev = models.CharField(max_length=256, default="")
     description = models.TextField(default="")
     cena = models.CharField(max_length=128, default="")
@@ -29,26 +32,22 @@ class Product(models.Model):
     is_published = models.BooleanField(default=True)
 
 
-class ProductAttributes(models.Model):
-    id = models.IntegerField(primary_key=True)
+class ProductAttributes(BaseModel):
     attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
-class Image(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Image(BaseModel):
     obrazek = models.TextField(default="")
 
 
-class ProductImage(models.Model):
-    id = models.IntegerField(primary_key=True)
+class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     obrazek_id = models.ForeignKey(Image, on_delete=models.CASCADE)
     nazev = models.CharField(max_length=256, default="")
 
 
-class Catalog(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Catalog(BaseModel):
     nazev = models.CharField(max_length=256, default="")
     obrazek_id = models.ForeignKey(Image, on_delete=models.CASCADE)
     products_ids = models.ManyToManyField(Product)
